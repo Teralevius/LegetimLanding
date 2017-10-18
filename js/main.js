f56a7bdafe1d73dd489f417ad817cea199a1879d
user.email <ilya.fedorovich.97@gmail.com>
2017-10-18 17:35:52 +0300

    mobile buttons listeners

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  console.log($("#owl-demo"));
+
     $("#owl-demo").owlCarousel({
 
         autoPlay: 3000, //Set AutoPlay to 3 seconds
@@ -20,5 +20,68 @@ $(document).ready(function() {
         }
 
     });
-
+    $('.buttonHeader').on('click',function () {
+        $('.formFon').css("display","flex");
+    });
+    $('.close').on('click',function () {
+        $('.formFon').css("display","none");
+    });
+    $('#mobileSkype').on('click',function () {
+        if($('#contactText').text()==$('#skype').text()){
+            $('.contactPanel').css("display", "none");
+            $('#contactText').html("");
+        }else{$('.contactPanel').css("display", "none");
+        $('#contactText').html($('#skype').text());
+        $('.contactPanel').css("display", "flex");}
+    });
+    $('#mobilePhone').on('click',function () {
+        if($('#contactText').text()== $('#phone').text()){
+            $('.contactPanel').css("display", "none");
+            $('#contactText').html("");
+        }else{
+        $('.contactPanel').css("display", "none");
+        $('#contactText').html($('#phone').text());
+        $('.contactPanel').css("display", "flex");
+        }
+    });
+    $('#mobileClock').on('click',function () {
+        if($('#contactText').text()== $('#time').text()){
+            $('.contactPanel').css("display", "none");
+            $('#contactText').html("");
+        }else{
+            $('.contactPanel').css("display", "none");
+            $('#contactText').html($('#time').text());
+            $('.contactPanel').css("display", "flex");
+        }
+    });
+    $('#mobileEnvelope').on('click',function () {
+        if($('#contactText').text()== $('#mail').text()){
+            $('.contactPanel').css("display", "none");
+            $('#contactText').html("");
+        }else{
+            $('.contactPanel').css("display", "none");
+            $('#contactText').html($('#mail').text());
+            $('.contactPanel').css("display", "flex");
+        }
+    });
+    $('#mobileHome').on('click',function () {
+        if($('#contactText').text()== $('#homephone').text()){
+            $('.contactPanel').css("display", "none");
+            $('#contactText').html("");
+        }else{
+            $('.contactPanel').css("display", "none");
+            $('#contactText').html($('#homephone').text());
+            $('.contactPanel').css("display", "flex");
+        }
+    });
+    $('#mobileMarker').on('click',function () {
+        if($('#contactText').text()== $('#adress').text()){
+            $('.contactPanel').css("display", "none");
+            $('#contactText').html("");
+        }else{
+            $('.contactPanel').css("display", "none");
+            $('#contactText').html($('#adress').text());
+            $('.contactPanel').css("display", "flex");
+        }
+    });
 });
